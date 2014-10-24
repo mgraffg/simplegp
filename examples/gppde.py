696560d2d820f6be4105db3dda4f7e191a22007e
Mario Graff <mgraffg@dep.fie.umich.mx>
2014-10-23 21:13:37 -0500

    Putting a seed

--- examples/gppde.py
+++ examples/gppde.py
@@ -10,6 +10,7 @@ X = np.vstack((x**2, x, np.ones(x.shape[0])))
 y = (X.T * pol).sum(axis=1)
 x = x[:, np.newaxis]
 gp = GPPDE(generations=30, verbose=True,
+           seed=2,
            # max_mem=10, verbose_nind=10,
            # update_best_w_rprop=True,
            max_length=1000).train(x, y)
