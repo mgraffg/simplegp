202b1e845edad7d5983bb7757c732e48431a09ed
Mario Graff <mgraffg@dep.fie.umich.mx>
2014-10-29 09:12:15 -0600

    Cleaning

--- SimpleGP/recursiveGP.py
+++ SimpleGP/recursiveGP.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import numpy as np
-from SimpleGP.RecursiveGP import Recursive
+from SimpleGP.Recursive import Recursive
 from SimpleGP.simplegp import GP
 
 
