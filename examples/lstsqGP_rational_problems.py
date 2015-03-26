a6cc0e66b337e6034548de5db00d08af85a11045
Mario Graff <mgraffg@ieee.org>
2015-03-26 16:57:48 -0600

    This is a novel algorithm called Least Squares GP.
    This is just a concept.

---
+++ examples/lstsqGP_rational_problems.py
@@ -0,0 +1,10 @@
+import numpy as np
+from SimpleGP import lstsqGP
+
+x = np.arange(-1, 1.1, 0.1)[:, np.newaxis]
+l = np.array(map(lambda x: map(float, x.split()),
+                 open('data/rational-problems.txt', 'r').readlines()))
+
+gp = lstsqGP(seed=0, func=['+', '-', '*', '/'], verbose=True,
+             pxo=0.5, generations=50).train(x, l[0])
+gp.run()
